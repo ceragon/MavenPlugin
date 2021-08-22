ae9f6e52e9064257dd5f11c8b9ca536c8f6441cc
wangkaiwu <wangxkaixwu@163.com>
2021-08-22 17:20:03 +0800

    提交临时测试版本

--- RPCGenerator/src/main/java/com/ceragon/mavenplugin/rpc/RPCGenerator.java
+++ RPCGenerator/src/main/java/com/ceragon/mavenplugin/rpc/RPCGenerator.java
@@ -1,22 +1,103 @@
 package com.ceragon.mavenplugin.rpc;
 
+import com.ceragon.mavenplugin.util.ClassUtil;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Mojo(name = "generator")
+/**
+ * @phase package
+ * @requiresDependencyResolution compile
+ */
 public class RPCGenerator extends AbstractMojo {
     @Parameter(defaultValue = "${project}")
     public MavenProject project;
 
     @Parameter(defaultValue = "${project.compileClasspathElements}", readonly = true, required = true)
     private List<String> compilePath;
+    private Log log;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        log = getLog();
+        MavenProject currentProject = project;
+        for (int i = 0; i < 10; i++) {
+            if (currentProject == null){
+                break;
+            }
+            log.info("the project is :"+currentProject);
+            currentProject = currentProject.getParent();
+        }
+        ClassLoader classLoader = getClassLoader(this.project);
+        ClassUtil classUtil = new ClassUtil(log, classLoader);
+        log.info("Generating");
+        try {
+            Class<?> annotation = classLoader.loadClass("com.ceragon.monkeygame.rpc.RpcDefine");
+            log.info("annotation:" + annotation);
+            log.info("test:" + classLoader.loadClass("com.ceragon.monkeygame.eventbus.EventChannelInactive"));
+            Set<Class<?>> rpcList = classUtil.scan("com.ceragon.monkeygame", annotation);
+            log.info("rpcList:" + rpcList);
+        } catch (ClassNotFoundException e) {
+            throw new MojoFailureException("load class failed", e);
+        }
+//        classUtil.scan("com.ceragon.monkeygame",)
+//        try {
+//            Class<?> type = getClassLoader(this.project).loadClass("com.ceragon.monkeygame.eventbus.EventChannelInactive");
+//            getLog().info("Generating:" + type);
+//        } catch (ClassNotFoundException e) {
+//            throw new MojoFailureException("load class failed", e);
+//        }
+        log.info("Finishing");
     }
+
+    private void addArtifactClasspath(MavenProject project, List<String> classpathElements) {
+//        classpathElements.addAll(project.getArtifacts().stream().
+//                map(artifact -> artifact.getFile().getAbsolutePath())
+//                .collect(Collectors.toList()));
+//        classpathElements.add(project.getArtifact().getFile().getAbsolutePath());
+        log.info("Adding classpath " + project.getArtifact().getClass());
+        MavenProject parent = project.getParent();
+        if (parent == null) {
+            return;
+        }
+        addArtifactClasspath(parent, classpathElements);
+    }
+
+    private ClassLoader getClassLoader(MavenProject project) {
+        try {
+            // 所有的类路径环境，也可以直接用 compilePath
+            List<String> classpathElements = project.getCompileClasspathElements();
+            classpathElements.add(project.getBuild().getOutputDirectory());
+            classpathElements.add(project.getBuild().getTestOutputDirectory());
+            addArtifactClasspath(project, classpathElements);
+            // 转为 URL 数组
+            URL[] urls = new URL[classpathElements.size()];
+            for (int i = 0; i < classpathElements.size(); ++i) {
+                urls[i] = new File(classpathElements.get(i)).toURL();
+                log.info("the url is " + urls[i]);
+            }
+
+            // 自定义类加载器
+            return new URLClassLoader(urls, this.getClass().getClassLoader());
+        } catch (Exception e) {
+            getLog().debug("Couldn't get the classloader.");
+            return this.getClass().getClassLoader();
+        }
+    }
+
+
 }
---
+++ common/src/main/java/com/ceragon/mavenplugin/util/ClassUtil.java
@@ -0,0 +1,164 @@
+
+package com.ceragon.mavenplugin.util;
+
+
+import org.apache.maven.plugin.logging.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * 类扫描工具类
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class ClassUtil {
+    private final Log log;
+    private final ClassLoader classLoader;
+
+    public ClassUtil(Log log, ClassLoader classLoader) {
+        this.log = log;
+        this.classLoader = classLoader;
+    }
+
+    /**
+     * 按类使用的注解扫描
+     *
+     * @param basePackagesStr 需要扫描的包，多个包使用“,”分隔
+     * @param annClazz        需要扫描的类必须使用的Runtime级别注解
+     * @return
+     */
+    public Set<Class<?>> scan(String basePackagesStr, Class<?> annClazz) {
+        log.info("Scan start");
+        URL url;
+        String protocol;
+        String filePath;
+        String packageName;
+        String packagePath;
+        Enumeration<URL> list;
+        Set<Class<?>> classes = new HashSet<>();
+        if (basePackagesStr == null) return classes;
+        String[] basePackages = basePackagesStr.split(",");
+        for (String basePackage : basePackages) {
+            packageName = basePackage;
+            if (packageName.endsWith(".")) {
+                packageName = packageName
+                        .substring(0, packageName.lastIndexOf('.'));
+            }
+            packagePath = packageName.replace('.', '/');
+            try {
+                list = classLoader.getResources(packagePath);
+                while (list.hasMoreElements()) {
+                    url = list.nextElement();
+                    protocol = url.getProtocol();
+                    if ("file".equals(protocol)) {
+                        filePath = URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8);
+                        Set<Class<?>> value = scanFiles(packageName, filePath, annClazz);
+                        if (value != null) {
+                            classes.addAll(value);
+                        }
+                    } else if ("jar".equals(protocol)) {
+                        classes.addAll(scanJar(packageName, url, annClazz));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return classes;
+    }
+
+    /**
+     * 扫描并加载目录中的Class文件
+     *
+     * @param packagePath 要扫描的包，如：com.server_game.model
+     * @param filePath    要扫描的Class文件路径，与包参数对应，如：file://D:/Common/bin/com/server_game/model
+     * @param annClazz    扫描的Class需使用的Runtime注解
+     * @return
+     */
+    private Set<Class<?>> scanFiles(String packagePath, String filePath, Class annClazz) {
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        File dir = new File(filePath);
+        if (!dir.exists() || !dir.isDirectory()) {
+            return null;
+        }
+        File[] dirfiles = dir.listFiles();
+        if (dirfiles == null) {
+            return null;
+        }
+        for (File file : dirfiles) {
+            if (file.isDirectory()) {
+                Set<Class<?>> value = scanFiles(packagePath + "." + file.getName(), file.getAbsolutePath(), annClazz);
+                if (value != null) {
+                    classes.addAll(value);
+                }
+            } else {
+                if (!file.getPath().endsWith("class")) continue;
+                String className = file.getName().substring(0, file.getName().length() - 6);
+                className = packagePath + '.' + className;
+                try {
+                    log.info("start load:" + className);
+                    Class clazz = classLoader.loadClass(className);
+                    log.info("load finish:" + clazz);
+                    Annotation ann = clazz.getAnnotation(annClazz);
+                    if (ann != null) {
+                        classes.add(clazz);
+                    }
+                } catch (Exception e) {
+                    if (log.isErrorEnabled()) log.error("加载类<" + className + ">出错", e);
+                }
+            }
+        }
+        return classes;
+    }
+
+    /**
+     * 扫描并加载JAR中的Class文件
+     *
+     * @param packagePath 要扫描的包，如：com.server_game.model
+     * @param url         要扫描的Jar文件路径，如：jar:file://D:/GameServer/lib/Common.jar
+     * @param annClazz    扫描的Class需使用的Runtime注解
+     * @return
+     */
+    private Set<Class<?>> scanJar(String packagePath, URL url, Class annClazz) {
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        packagePath = packagePath.replace('.', '/');
+        JarFile jar;
+        try {
+            jar = ((JarURLConnection) url.openConnection()).getJarFile();
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+                if (!name.startsWith(packagePath) || entry.isDirectory()) {
+                    continue;
+                }
+
+                String className = name.replace('/', '.');
+                className = className.substring(0, className.length() - 6);
+                try {
+                    Class clazz = classLoader.loadClass(className);
+                    Annotation ann = clazz.getAnnotation(annClazz);
+                    if (ann != null) {
+                        classes.add(clazz);
+                    }
+                } catch (Exception e) {
+                    if (log.isErrorEnabled()) log.error("从包<" + url + ">加载类<" + className + ">出错", e);
+                }
+            }
+        } catch (IOException e) {
+            if (log.isErrorEnabled()) log.error("读取包<" + url + ">出错", e);
+        }
+        return classes;
+    }
+}
